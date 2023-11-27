5dce57de67eee236334ea034cd2af04c95cd4e2a
Manuel Figueroa <mfigueroaflores96@gmail.com>
2023-11-27 18:34:59 -0300

    Cargando el proyecto al repositorio

---
+++ blog/admin.py
@@ -0,0 +1,6 @@
+from django.contrib import admin
+from .models import Game, Opinion, Comentario 
+
+admin.site.register(Game)
+admin.site.register(Opinion)
+admin.site.register(Comentario)
---
+++ blog/models.py
@@ -0,0 +1,35 @@
+from typing import Any
+from django.db import models
+from django.db.models.fields import CharField, DateField
+from django.core.validators import MaxValueValidator
+from django import forms
+
+class Game(models.Model):
+    CONSOLE_CHOICES = [
+        ('Xbox', 'Xbox'),
+        ('PC', 'PC'),
+        ('Nintendo Switch', 'Nintendo Switch'),
+        ('PS5', 'PS5'),
+        ('Sin Información', 'Sin Información')
+    ]
+
+    title = models.CharField(max_length=100)
+    description = models.TextField(max_length=350)
+    console = models.CharField(max_length=20, choices=CONSOLE_CHOICES)
+    release_date = models.DateField()
+
+    def __str__(self):
+        return self.title
+
+
+class Opinion(models.Model):
+    game = models.ForeignKey(Game, on_delete=models.CASCADE)
+    rating = models.PositiveIntegerField(validators=[MaxValueValidator(5)])
+    opinion = models.TextField(max_length=300, null=True, blank=True)
+
+class Comentario(models.Model):
+    autor_name = models.CharField(max_length=100)
+    comentario = models.CharField(max_length=150)
+    date = models.DateField()
+
+ 
